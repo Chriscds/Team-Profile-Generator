e054e709e6ff371cb7531d816811df8a3d757bf3
CDS-admin1 <csouthon44@gmail.com>
2023-02-24 15:00:44 +0000

    Building prompts in index.js

--- index.js
+++ index.js
@@ -14,6 +14,44 @@ const render = require("./src/page-template.js");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 // inquirer prompt questions
+inquirer.prompt([{
+    // manager questions
+}]).then(response => {
+    // populate manager information
+    // prompt for next employee()
+})
 
-// mananger
-// 
+const NextEmployeePrompt = () => {
+    inquirer.prompt([{
+        // Choice for employee
+    }]).then(response => {
+        // if engineer
+            // promptForEngineer
+        // else if intern
+            // promptForIntern
+        // else
+            // use the functionality from page-template to generate the team
+    })
+}
+
+const promptForEngineer = () => {
+    inquirer.prompt([{
+        //engineer questions
+    }]).then(response => {
+        // add new engineer to employees array
+            // promptForNextEmployee
+    })
+}
+
+const promptForIntern = () => {
+    inquirer.prompt([{
+        //intern questions
+    }]).then(response => {
+        // add new intern to employees array
+            // promptForNextEmployee
+    })
+}
+
+const buildPage = () => {
+
+}
