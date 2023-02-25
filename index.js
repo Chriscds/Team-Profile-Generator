6981a71ea687790a1d682d6ef85d153e52a167f8
CDS-admin1 <csouthon44@gmail.com>
2023-02-25 16:47:55 +0000

    Included special characters in question strings

--- index.js
+++ index.js
@@ -20,25 +20,25 @@ inquirer.prompt([
         // What is the team manager's name?
         type: 'input',
         name: 'name',
-        message: 'What is the team managers name?',
+        message: 'What is the team manager\'s name?',
     },
     {
             // What is the team manager's ID?
         type: 'input',
         name: 'id',
-        message: 'What is the team managers ID?',
+        message: 'What is the team manager\'s ID?',
     },
     {
                 // What is the team manager's email address?
         type: 'input',
         name: 'email',
-        message: 'What is the team managers email address?',
+        message: 'What is the team manager\'s email address?',
     },
     {
                     // What is the team manager's office number?
         type: 'input',
         name: 'number',
-        message: 'What is the team managers office number?',
+        message: 'What is the team manager\'s office number?',
 
         // What team member would you like to add? (Choice for employee)
 }]).then(response => {
@@ -54,7 +54,7 @@ const NextEmployeePrompt = () => {
         type: 'list',
         message: 'Would you like to add another employee?',
         name: 'employee',
-        choices: ['Engineer', 'Intern', 'Finished with team input'],
+        choices: ['Engineer', 'Intern', 'No more Employee\'s'],
     }]).then(response => {
         
     const employeeChoice = response.employee;
@@ -79,25 +79,25 @@ const promptForEngineer = () => {
             // What is your Engineers name?
             type: 'input',
             name: 'engineerName', //Check name is good to use
-            message: 'What is your Engineers name?',
+            message: 'What is your Engineer\'s name?',
         },
         {
                 // What is your Engineers ID?
             type: 'input',
             name: 'engineerId', //Check name is good to use
-            message: 'What is your Engineers ID?',
+            message: 'What is your Engineer\'s ID?',
         },
         {
                     // What is your Engineers email address?
             type: 'input',
             name: 'engineeremail', //Check name is good to use
-            message: 'What is your Engineers email address?',
+            message: 'What is your Engineer\'s email address?',
         },
         {
                         // What is your Engineers GitHub username?
             type: 'input',
             name: 'githubUsername', //Check name is good to use
-            message: 'What is your Engineers GitHub username?',        
+            message: 'What is your Engineer\'s GitHub username?',        
          
             // What team member would you like to add? (Choice for employee)
     }]).then(response => {
@@ -116,19 +116,19 @@ const promptForIntern = () => {
         // What is your Intern's name?
         type: 'input',
             name: 'internName', //Check name is good to use
-            message: 'What is your Interns name?',
+            message: 'What is your Intern\'s name?',
         },
         {
             // What is your Intern's ID?
             type: 'input',
             name: 'internId', //Check name is good to use
-            message: 'What is your Interns ID?',
+            message: 'What is your Intern\'s ID?',
         },
         {
                 // What is your Intern's email address?
             type: 'input',
             name: 'internemail', //Check name is good to use
-            message: 'What is your Interns email address?',
+            message: 'What is your Intern\'s email address?',
         
 
             // What team member would you like to add? (Choice for employee)
